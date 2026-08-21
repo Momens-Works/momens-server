913aec2e71cd1eee95e5dec4ac49ad1558fbb35c
Kimgyuilli <rlarbdlf222@naver.com>
2026-08-21 13:04:49 +0900

    style(MOM-0867/web): Optional import를 정리합니다

--- modules/project/src/main/java/works/momens/server/project/task/TaskUpdateRepository.java
+++ modules/project/src/main/java/works/momens/server/project/task/TaskUpdateRepository.java
@@ -1,6 +1,7 @@
 package works.momens.server.project.task;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,5 @@ interface TaskUpdateRepository extends JpaRepository<TaskUpdate, UUID> {
 
   List<TaskUpdate> findByTaskIdAndDeletedAtIsNullOrderByCreatedAtAscIdAsc(UUID taskId);
 
-  java.util.Optional<TaskUpdate> findByIdAndTaskIdAndDeletedAtIsNull(UUID id, UUID taskId);
+  Optional<TaskUpdate> findByIdAndTaskIdAndDeletedAtIsNull(UUID id, UUID taskId);
 }
